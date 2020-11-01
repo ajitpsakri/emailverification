e444ce9b4863b01634b83760e611f0db0587ebc6
ajitpsakri <smartsakri@gmail.com>
2020-11-01 15:04:52 +0530

    fixed CORS Problem

--- src/EmailVerificationComponent.js
+++ src/EmailVerificationComponent.js
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
-
+// import axios from "axios";
 const EmailVerificationComponent = () => {
   const [inputEmail, setInputEmail] = useState("");
   const handleChange = (e) => {
     setInputEmail(e.target.value);
   };
+  let key = "RMOAD7L9PEIZMV5ZS2CK";
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch(`/ajit`, {
-      method: "get",
-      mode: "cors",
-      Get: "/users",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.mailboxvalidator.com/v1/validation/single?email=${inputEmail}&key=${key}`,
+      {
+        method: "get",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
       .then((resp) => resp.json())
       .then((data) => console.log(data))
       .catch((error) => console.log(error));
+
+    // axios
+    //   .all([
+    //     axios.get(
+    //       `https://cors-anywhere.herokuapp.com/https://api.mailboxvalidator.com/v1/validation?email=${inputEmail}&key=${key}`
+    //     ),
+    //     {
+    //       origin: "https://api.mailboxvalidator.com/v1/validation",
+    //     },
+    //   ])
+    //   .then((resp) => console.log(resp));
   };
   return (
     <form onSubmit={handleSubmit}>
